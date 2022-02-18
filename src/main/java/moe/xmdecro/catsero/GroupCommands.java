d008067af6e59848feffee47dcdc8dbae5e7cd42
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-02-18 20:50:59 +0800

    稍微让我测试一下（

--- src/main/java/moe/xmdecro/catsero/GroupCommands.java
+++ src/main/java/moe/xmdecro/catsero/GroupCommands.java
@@ -7,25 +7,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration;
+import org.bukkit.ChatColor;
 
-public class CommandExample extends JavaPlugin implements Listener {
-
-    @Override // 加载插件
-    public void onLoad(PluginLogger logger) { }
-
-    @Override // 启用插件
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
-
-    @Override // 禁用插件
-    public void onDisable() { }
+public class GroupCommands extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onFriendMessageReceive(MiraiFriendMessageEvent e){
         if(e.getMessage().equals("ban")) {
-            MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("当前在线人数：" + Bukkit.getServer().getOnlinePlayers().size()+"人");
+            //getBot(e.getBotID()).getFriend(e.getSenderID()));
+            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("待实现");
         }
     }
 
--- src/main/java/moe/xmdecro/catsero/Main.java
+++ src/main/java/moe/xmdecro/catsero/Main.java
@@ -4,37 +4,46 @@ import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bukkit.event.MiraiFriendMessageEvent;
 import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginLogger;
-
-import java.util.logging.Logger;
 
 public class Main extends JavaPlugin implements Listener {
 
+    @Override // 加载插件
+    public void onLoad() {
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', "CatSero加载中..."));
+    }
+
     @Override // 启用插件
     public void onEnable() {
+        this.getCommand("catsero").setExecutor(new CatSero());
         Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', "插件加载完毕！"));
     }
 
     @Override // 禁用插件
     public void onDisable() {
-
-    }
-
-    @EventHandler
-    public void onFriendMessageReceive(MiraiFriendMessageEvent e) {
-        if () {
-            getLogger().info("接收到好友" + e.getSenderID() + "的消息: " + e.getMessage());
-            MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("你发送了一条消息：" + e.getMessage());
-        }
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', "&c关闭CatSero"));
     }
 
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
-        getLogger().info("接收到群聊"+e.getGroupID()+"的消息: "+e.getMessage());
-        MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessageMirai("[mirai:at:"+e.getSenderID()+"] 你发送了一条消息："+e.getMessage());
+
+        //getLogger().info("接收到群聊"+e.getGroupID()+"的消息: "+e.getMessage());
+        //MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessageMirai("[mirai:at:"+e.getSenderID()+"] 你发送了一条消息："+e.getMessage());
+    }
+    public class CatSero implements CommandExecutor{
+        // This method is called, when somebody uses our command
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e------CatSero菜单------"));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "/catsero reload") + " 重载插件");
+            return true;
+        }
     }
 }
