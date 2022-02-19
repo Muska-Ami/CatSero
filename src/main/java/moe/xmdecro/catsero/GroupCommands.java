eb0802d9ffaffd86818b87473ce137861bce25a5
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-02-19 13:04:51 +0800

    更新pom.xml，尝试构建

--- src/main/java/moe/xmdecro/catsero/GroupCommands.java
+++ src/main/java/moe/xmdecro/catsero/GroupCommands.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
 
 public class GroupCommands extends JavaPlugin implements Listener {
 
@@ -15,7 +14,7 @@ public class GroupCommands extends JavaPlugin implements Listener {
     public void onFriendMessageReceive(MiraiFriendMessageEvent e){
         if(e.getMessage().equals("ban")) {
             //getBot(e.getBotID()).getFriend(e.getSenderID()));
-            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("待实现");
+            MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("待实现");
         }
     }
 
