109f071e6289b3ba09ba1afd7bc997d140147743
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-02-19 22:32:23 +0800

    尝试添加更多Code

--- src/main/java/moe/xmdecro/catsero/GroupCommands.java
+++ src/main/java/moe/xmdecro/catsero/GroupCommands.java
@@ -3,23 +3,16 @@ package moe.xmdecro.catsero;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GroupCommands extends JavaPlugin implements Listener {
 
-    @Override // 加载插件
-    public void onLoad() { }
-
     @Override // 启用插件
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
-
-    @Override // 禁用插件
-    public void onDisable() { }
 
     /*@EventHandler
     public void onFriendMessageReceive(MiraiFriendMessageEvent e){
@@ -32,6 +25,7 @@ public class GroupCommands extends JavaPlugin implements Listener {
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
         if (e.getMessage().equals("cs online")) {
+            //
             MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("当前在线人数：" + Bukkit.getServer().getOnlinePlayers().size()+"人");
         }
         if (e.getMessage().equals("cs pica")) {
--- src/main/java/moe/xmdecro/catsero/Main.java
+++ src/main/java/moe/xmdecro/catsero/Main.java
@@ -1,14 +1,11 @@
 package moe.xmdecro.catsero;
 
 import me.dreamvoid.miraimc.api.MiraiBot;
-import me.dreamvoid.miraimc.bukkit.event.MiraiFriendMessageEvent;
-import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -31,18 +28,22 @@ public class Main extends JavaPlugin implements Listener {
         Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', "[&bCatSero&r] &c关闭CatSero"));
     }
 
-    @EventHandler
-    public void onGroupMessageReceive(MiraiGroupMessageEvent e){
+    /*public void onGroupMessageReceive(MiraiGroupMessageEvent e){
 
         //getLogger().info("接收到群聊"+e.getGroupID()+"的消息: "+e.getMessage());
         //MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessageMirai("[mirai:at:"+e.getSenderID()+"] 你发送了一条消息："+e.getMessage());
-    }
+    }*/
     public class CatSero implements CommandExecutor{
         // This method is called, when somebody uses our command
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e------CatSero菜单------"));
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "/catsero reload") + " 重载插件");
+            if (args == null) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e------&bCatSero菜单&e------"));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7/catsero sendmsg   &b发送群消息"));
+            } else if (args[0] == "sendmsg") {
+                MiraiBot.getBot(Long.parseLong(args[1])).getGroup(Long.parseLong(args[2])).sendMessage(args[3]);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "[&bCatSero&r] &e已发送群消息"));
+            }
             return true;
         }
     }
