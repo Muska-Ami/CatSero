e309b93dc9360879fb38e7608475d5931a9f4e73
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-02-19 21:04:20 +0800

    删掉一行import

--- src/main/java/moe/xmdecro/catsero/GroupCommands.java
+++ src/main/java/moe/xmdecro/catsero/GroupCommands.java
@@ -7,8 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-
 public class GroupCommands extends JavaPlugin implements Listener {
 
     /*@EventHandler
