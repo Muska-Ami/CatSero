cb3c69344444fd89b564c9d2f66ce9181171560c
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-04-02 22:09:46 +0800

    Update Loader.java

--- src/main/java/moe/xmcn/catsero/Loader.java
+++ src/main/java/moe/xmcn/catsero/Loader.java
@@ -1,2 +1,7 @@
 package moe.xmcn.catsero;
 
+import moe.xmcn.catsero.utils.Metrics;
+
+import moe.xmcn.catsero.bStats;
+import moe.xmcn.catsero.villa.Main;
+import moe.xmcn.catsero.extra.chatForward.BukkitPlugin;
