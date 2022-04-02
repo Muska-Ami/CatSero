5c3759fa86df2f2866f4bea3f0c2d3f02f1852b7
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-04-02 22:50:08 +0800

    Update onPlayerJoin.java

--- src/main/java/moe/xmcn/catsero/extra/chatForward/listener/onPlayerJoin.java
+++ src/main/java/moe/xmcn/catsero/extra/chatForward/listener/onPlayerJoin.java
@@ -22,20 +22,7 @@ public class onPlayerJoin implements Listener {
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    String message = plugin.getConfig().getString("bot.player-join-message").replace("%player%", e.getPlayer().getName());
-                    plugin.getConfig().getLongList("bot.bot-accounts").forEach(bot -> plugin.getConfig().getLongList("bot.group-ids").forEach(group -> {
-                        try {
-                            MiraiBot.getBot(bot).getGroup(group).sendMessageMirai(message);
-                        } catch (NoSuchElementException e) {
-                            if (MiraiHttpAPI.Bots.containsKey(bot)) {
-                                try {
-                                    MiraiHttpAPI.INSTANCE.sendGroupMessage(MiraiHttpAPI.Bots.get(bot), group, message);
-                                } catch (IOException | AbnormalStatusException ex) {
-                                    plugin.getLogger().warning("使用" + bot + "发送消息时出现异常，原因: " + ex);
-                                }
-                            } else plugin.getLogger().warning("指定的机器人" + bot + "不存在，是否已经登录了机器人？");
-                        }
-                    }));
+                    MiraiBot.getBot(plugin.getConfig().getLong("bot.botaccount")).getGroup(plugin.getConfig().getLong("bot.groupid")).sendMessageMirai(plugin.getConfig().getString("bot.player-join-message").replace("%player%",e.getPlayer().getName()));
                 }
             }.runTaskAsynchronously(plugin);
         }
