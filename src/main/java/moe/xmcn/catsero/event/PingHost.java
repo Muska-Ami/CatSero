2ce595398034f9b852f949380f5a285c5111ed3b
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-04-03 14:08:43 +0800

    稍微改下结构，暂时注释群代码

--- src/main/java/moe/xmcn/catsero/event/PingHost.java
+++ src/main/java/moe/xmcn/catsero/event/PingHost.java
@@ -6,37 +6,44 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class PingHost implements Listener, CommandExecutor {
 
-    public String PingTool(String pingaddress) throws IOException, InterruptedException {
-        InetAddress address = InetAddress.getByName(pingaddress);
-        int flag=0;
-        for (int i = 0; i < 4; i++) {
-            boolean b=address.isReachable(1000);
-            if(b)
-                flag++;
-            Thread.sleep(1000);
-        }
-        return String.valueOf(flag);
-    }
-
+    Plugin plugin = moe.xmcn.catsero.Main.getPlugin(moe.xmcn.catsero.Main.class);
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 2) {
             if (args[0] == "ping") {
+                InetAddress address = null;
                 try {
-                    int res = Integer.parseInt(PingTool(args[1]));
-                    sender.sendMessage(args[1]+" 的  Ping 统计信息：\n   数据包：已发送 = 4， 已接收 = "+res+" ,丢失 = "+(4-res)+"("+(4-res)/4*100+"% 丢失)");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
+                    address = InetAddress.getByName(args[1]);
+                } catch (UnknownHostException e) {
                     e.printStackTrace();
                 }
+                int flag=0;
+                for (int i = 0; i < 4; i++) {
+                    boolean b= false;
+                    try {
+                        b = address.isReachable(1000);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if(b)
+                        flag++;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                sender.sendMessage(args[1] + " 的  Ping 统计信息：\n   数据包：已发送 = 4， 已接收 = " + flag + " ,丢失 = " + (4-flag) + "(" + (4-flag)/4*100 + "% 丢失)");
+
             } else {
                 return false;
             }
@@ -46,13 +53,13 @@ public class PingHost implements Listener, CommandExecutor {
         return false;
     }
 
-    @EventHandler
-    public void onGroupMessageReceive(MiraiGroupMessageEvent e) {
+    //@EventHandler
+    //public void onGroupMessageReceive(MiraiGroupMessageEvent e) {
 
-        if (e.getBotID() == e.getBotID()) {
+        //if (e.getBotID() == plugin.getConfig().getString("bot")) {
 
-        }
+        //}
         //MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage(address.getHostAddress()+" 的  Ping 统计信息：\n   数据包：已发送 = 4， 已接收 = "+flag+" ,丢失 = "+(4-flag)+"("+(4-flag)/4*100+"% 丢失)");
-    }
+    //}
 
 }
