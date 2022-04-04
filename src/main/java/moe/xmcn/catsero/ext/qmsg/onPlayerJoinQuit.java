a8fde84019f3045cacc6be25a1a8e7d5767ed2fb
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-04-04 16:25:23 +0800

    将PunyCode编码作为单独类 添加玩家加入/退出消息转发

--- src/main/java/moe/xmcn/catsero/ext/qmsg/onPlayerJoinQuit.java
+++ src/main/java/moe/xmcn/catsero/ext/qmsg/onPlayerJoinQuit.java
@@ -8,9 +8,11 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 public class onPlayerJoinQuit implements Listener {
+
     Plugin plugin = moe.xmcn.catsero.Main.getPlugin(moe.xmcn.catsero.Main.class);
+
     @EventHandler
-    public void onPlayerJoinQuit(PlayerQuitEvent plqev, PlayerJoinEvent pljev) {
+    public void onPlayerQuit(PlayerJoinEvent pljev) {
         if (plugin.getConfig().getString("general.ext-qmsg.send-play-quit-join-message.enabled") == "true") {
             Long bot = Long.valueOf(plugin.getConfig().getString("general.bot"));
             Long group = Long.valueOf(plugin.getConfig().getString("general.group"));
@@ -18,8 +20,16 @@ public class onPlayerJoinQuit implements Listener {
             String pljname = pljev.getPlayer().getName();
             String joinmsg = plugin.getConfig().getString("general.ext-qmsg.send-play-quit-join-message.format.join").replace("%player%", pljname);
             MiraiBot.getBot(bot).getGroup(group).sendMessageMirai(joinmsg);
+        }
+    }
 
-            String plqname = pljev.getPlayer().getName();
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent plqev) {
+        if (plugin.getConfig().getString("general.ext-qmsg.send-play-quit-join-message.enabled") == "true") {
+            Long bot = Long.valueOf(plugin.getConfig().getString("general.bot"));
+            Long group = Long.valueOf(plugin.getConfig().getString("general.group"));
+
+            String plqname = plqev.getPlayer().getName();
             String quitmsg = plugin.getConfig().getString("general.ext-qmsg.send-play-quit-join-message.format.quit").replace("%player%", plqname);
             MiraiBot.getBot(bot).getGroup(group).sendMessageMirai(quitmsg);
         }
---
+++ src/main/java/moe/xmcn/catsero/utils/Punycode.java
@@ -0,0 +1,147 @@
+package moe.xmcn.catsero.utils;
+
+
+public class Punycode {
+    private static int TMIN = 1;
+    private static int TMAX = 26;
+    private static int BASE = 36;
+    private static int INITIAL_N = 128;
+    private static int INITIAL_BIAS = 72;
+    private static int DAMP = 700;
+    private static int SKEW = 38;
+    private static char DELIMITER = '-';
+
+    public static String encodeURL(String url) {
+        if (!url.contains("."))
+            return url;
+        String mainContent = url.substring(0, url.lastIndexOf("."));
+        String prefix = mainContent.contains(".") ? mainContent.substring(0, mainContent.lastIndexOf(".") + 1) : "";
+        if (mainContent.contains("."))
+            mainContent = mainContent.substring(mainContent.lastIndexOf(".") + 1);
+        mainContent = Punycode.encode(mainContent, "xn--");
+        String suffix = url.substring(url.lastIndexOf("."));
+        return prefix + mainContent + suffix;
+    }
+
+    /**
+     *
+     * Punycodes a unicode string. THIS IS NOT SUITABLE FOR UNICODE AND LETTER
+     * MIXING
+     *
+     * @param input Unicode string.
+     *
+     * @return Punycoded string, but original text for throw an exception
+     *
+     */
+    public static String encode(String input) {
+        return Punycode.encode(input, "");
+    }
+
+    /**
+     *
+     * Punycodes a unicode string. THIS IS NOT SUITABLE FOR UNICODE AND LETTER
+     * MIXING
+     *
+     * @param input Unicode string.
+     *
+     * @return Punycoded string, but original text for throw an exception
+     *
+     */
+    public static String encode(String input, String successPrefix) {
+        int n = INITIAL_N;
+        int delta = 0;
+        int bias = INITIAL_BIAS;
+        StringBuilder output = new StringBuilder();
+        int b = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (isBasic(c)) {
+                output.append(c);
+                b++;
+            }
+        }
+        if(b >= input.length()) return output.toString();
+        if (b > 0) {
+            output.append(DELIMITER);
+        }
+        int h = b;
+        while (h < input.length()) {
+            int m = Integer.MAX_VALUE;
+            for (int i = 0; i < input.length(); i++) {
+                int c = input.charAt(i);
+                if (c >= n && c < m) {
+                    m = c;
+                }
+            }
+            if (m - n > (Integer.MAX_VALUE - delta) / (h + 1)) {
+                return input;
+            }
+            delta = delta + (m - n) * (h + 1);
+            n = m;
+            for (int j = 0; j < input.length(); j++) {
+                int c = input.charAt(j);
+                if (c < n) {
+                    delta++;
+                    if (0 == delta) {
+                        return input;
+                    }
+                }
+                if (c == n) {
+                    int q = delta;
+                    for (int k = BASE;; k += BASE) {
+                        int t;
+
+                        if (k <= bias) {
+                            t = TMIN;
+                        } else if (k >= bias + TMAX) {
+                            t = TMAX;
+                        } else {
+                            t = k - bias;
+                        }
+                        if (q < t) {
+                            break;
+                        }
+                        output.append((char) digit2codepoint(t + (q - t) % (BASE - t)));
+                        q = (q - t) / (BASE - t);
+                    }
+                    output.append((char) digit2codepoint(q));
+                    bias = adapt(delta, h + 1, h == b);
+                    delta = 0;
+                    h++;
+                }
+            }
+            delta++;
+            n++;
+        }
+        output.insert(0, successPrefix);
+        return output.toString();
+    }
+    private static int adapt(int delta, int numpoints, boolean first) {
+        if (first) {
+            delta = delta / DAMP;
+        } else {
+            delta = delta / 2;
+        }
+        delta = delta + (delta / numpoints);
+        int k = 0;
+        while (delta > ((BASE - TMIN) * TMAX) / 2) {
+            delta = delta / (BASE - TMIN);
+            k = k + BASE;
+        }
+        return k + ((BASE - TMIN + 1) * delta) / (delta + SKEW);
+    }
+
+    private static boolean isBasic(char c) {
+        return c < 0x80;
+    }
+
+    private static int digit2codepoint(int d) {
+        if (d < 26) {
+            return d + 'a';
+        } else if (d < 36) {
+            return d - 26 + '0';
+        } else {
+            return d;
+        }
+    }
+}
