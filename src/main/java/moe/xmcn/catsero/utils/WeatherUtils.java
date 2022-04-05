fe36f43b0ffeac6fb1cb9b8301fb781ee55890c8
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-04-05 10:18:05 +0800

    更新Utils

--- src/main/java/moe/xmcn/catsero/utils/WeatherUtils.java
+++ src/main/java/moe/xmcn/catsero/utils/WeatherUtils.java
@@ -14,6 +14,62 @@ import java.util.zip.GZIPInputStream;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
  
+ public class WeatherInfo {
+	private String date;//时间
+	private String cityname;//城市名
+	private String weather;//天气
+	private String temperature;//气温
+	private String airquality;//pm2.5
+ 
+	public String getDate() {
+		return date;
+	}
+ 
+	public void setDate(String date) {
+		this.date = date;
+	}
+ 
+	public String getCityname() {
+		return cityname;
+	}
+ 
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+ 
+	public String getWeather() {
+		return weather;
+	}
+ 
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+ 
+	public String getTemperature() {
+		return temperature;
+	}
+ 
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+ 
+	public String getAirquality() {
+		return airquality;
+	}
+ 
+	public void setAirquality(String airquality) {
+		this.airquality = airquality;
+	}
+ 
+	@Override
+	public String toString() {
+		return "WeatherInfo [date=" + date + ", cityname=" + cityname
+				+ ", weather=" + weather + ", temperature=" + temperature
+				+ ", airquality=" + airquality + "]";
+	}
+	
+}
+ 
 /**
  * 通过get请求向网站http://wthrcdn.etouch.cn/weather_mini获取某个 城市的天气状况数据，数据格式是Json
  * 
