4f66fff6d639c6c5194a66b162c309384b929905
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-04-30 13:34:30 +0800

    添加自定义玩家加入/退出消息 添加自定义玩家加入/退出消息 更新README.md

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ SpigotAPI版本: 1.12.2-R0.1-SNAPSHOT
 构建结果请前往Actions - [Build](https://github.com/XiaMoHuaHuo-CN/CatSero/actions/workflows/builder.yml)
 
 ## 未来功能
+
+### 插件功能
 - [ ] QQ群-Minecraft消息同步
 - [x] Minecraft玩家加入/退出通知到QQ
 - [ ] QQ群踢人
@@ -13,6 +15,11 @@ SpigotAPI版本: 1.12.2-R0.1-SNAPSHOT
 - [x] Ping功能(Minecraft内/QQ群内)
 - [ ] QQ群给予OP
 - [x] 天气获取
+- [ ] 迎新功能
+- [x] 自定义玩家加入/退出游戏全体消息
+
+### 其他
+- [ ] 自动检查更新
 
 # bStats
 ![](https://bstats.org/signatures/bukkit/CatSero.svg)
---
+++ src/main/java/moe/xmcn/catsero/event/listener/JoinQuitMessage/onPlayerQuit.java
@@ -0,0 +1,28 @@
+package moe.xmcn.catsero.event.listener.JoinQuitMessage;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+
+public class onPlayerQuit implements Listener {
+
+    Plugin plugin = moe.xmcn.catsero.Main.getPlugin(moe.xmcn.catsero.Main.class);
+    File usc = new File(plugin.getDataFolder(), "usesconfig.yml");
+    FileConfiguration usesconfig = YamlConfiguration.loadConfiguration(usc);
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (usesconfig.getBoolean("join-quit-message.enabled")) {
+            String msg = usesconfig.getString("join-quit-message.format.quit");
+            msg = msg.replace("%player%", event.getPlayer().getDisplayName());
+            msg = ChatColor.translateAlternateColorCodes('&', msg);
+            event.setJoinMessage(msg);
+        }
+    }
+}
