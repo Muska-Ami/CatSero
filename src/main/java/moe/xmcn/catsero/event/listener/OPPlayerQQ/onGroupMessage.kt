2a6b1bbb7fdabd8feddf1667558f0594f972982c
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-05-04 11:27:56 +0800

    [ci skip]删除无用的import

--- src/main/java/moe/xmcn/catsero/event/listener/OPPlayerQQ/onGroupMessage.kt
+++ src/main/java/moe/xmcn/catsero/event/listener/OPPlayerQQ/onGroupMessage.kt
@@ -5,7 +5,6 @@ import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent
 import moe.xmcn.catsero.event.gist.PlayerUUID
 import moe.xmcn.catsero.utils.Config
 import org.bukkit.Bukkit
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.permissions.ServerOperator
