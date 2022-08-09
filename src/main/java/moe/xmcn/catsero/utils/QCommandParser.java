5ac4f219fce414835f4d6f0ac30e37c75e5b4678
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-08-09 19:13:08 +0800

    [ci skip]写了个命令解析器

---
+++ src/main/java/moe/xmcn/catsero/utils/QCommandParser.java
@@ -0,0 +1,39 @@
+package moe.xmcn.catsero.utils;
+
+public interface QCommandParser {
+
+    class getParser {
+
+        /**
+         * 解析QQ群命令
+         * @param message   消息
+         * @return  数组或null
+         */
+        public String[] parse(String message) {
+            String pmh = parseCommandHeader(message);
+            if (pmh != null) {
+                return pmh.split(" ");
+            } else {
+                return null;
+            }
+        }
+
+        /**
+         * 判断是否有命令头，并处理命令头
+         * @param message   消息
+         * @return  处理后的消息或null
+         */
+        private String parseCommandHeader(String message) {
+            if (message.startsWith("!catsero")) {
+                message = message.replaceFirst("/catsero ", "");
+                return message;
+            } else if (message.startsWith("/catsero")) {
+                message = message.replaceFirst("!catsero ", "");
+                return message;
+            }
+            return null;
+        }
+
+    }
+
+}
