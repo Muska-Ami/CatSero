343c4b558111a6d6ecd96abb828dea33b2cc9676
XiaMoHuaHuo_CN <mail@huahuo-cn.tk>
2023-01-27 04:54:59 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.8.255-20230104.204420-1445"
+    kotlin("jvm") version "1.8.0"
 }
 
 group = "moe.xmcn.catsero"
